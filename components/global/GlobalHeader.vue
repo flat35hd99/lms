03d1c27815ce5d599939374c3acfe881a452ea11
flat35hd99 <flat35hd99@gmail.com>
2021-10-19 07:48:41 +0000

    WIP Add header

--- components/global/GlobalHeader.vue
+++ components/global/GlobalHeader.vue
@@ -1,4 +1,62 @@
 <template>
   <div>
+    <v-app-bar>
+      <v-app-bar-nav-icon class="d-md-none" @click.stop="drawer = !drawer" />
+      <v-toolbar-title>
+        <nuxt-link to="/">
+          <v-img
+            src="favicon.ico"
+            height="20"
+            contain
+          />
+        </nuxt-link>
+      </v-toolbar-title>
+      <v-tabs class="d-none d-md-flex">
+        <v-tab
+          v-for="link in links"
+          :key="link.name"
+          :to="link.path"
+          nuxt
+        >
+          {{ link.name }}
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      temporary
+    >
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.name"
+          :to="link.key"
+          nuxt
+        >
+          {{ link.name }}
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {
+          link: '/posts',
+          name: 'Posts'
+        },
+        {
+          link: '/machines/3d-printer',
+          name: 'Machine'
+        }
+      ]
+    }
+  }
+}
+</script>

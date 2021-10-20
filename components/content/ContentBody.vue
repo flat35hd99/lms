3b254d5712531fb75f7245a14132ad6ae1e8816e
flat35hd99 <flat35hd99@gmail.com>
2021-10-20 07:06:56 +0000

    Update content view

--- components/content/ContentBody.vue
+++ components/content/ContentBody.vue
@@ -14,3 +14,45 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.article-title {
+  font-size: 1.4rem;
+}
+.creation_date {
+  font-size: 0.7rem
+}
+.headline {
+  border-bottom: solid 1px rgb(221,221,221);
+  margin-top: 49px;
+  margin-bottom: 24px;
+}
+.nuxt-content {
+  h1 {
+    @extend .headline;
+    font-size: 1.25rem;
+    font-weight: 400;
+  }
+  h2 {
+    font-size: 1.15rem;
+    font-weight: 400;
+  }
+  h3 {
+    font-size: 1.05rem;
+    font-weight: 500;
+  }
+  h4 {
+    font-size: 0.95;
+    font-weight: 600;
+  }
+  p {
+    line-height: 2.0rem;
+    img {
+      max-width: 100%;
+      height: auto;
+      display: block;
+      margin: auto;
+      text-align: center;
+    }
+  }
+}
+</style>

366cf565b21305b4ac831c1ef7f944aefc0671f3
flat35hd99 <flat35hd99@gmail.com>
2021-10-20 07:54:54 +0000

    Fix header links

--- components/global/GlobalHeader.vue
+++ components/global/GlobalHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-app-bar>
+    <v-app-bar app>
       <v-app-bar-nav-icon class="d-md-none" @click.stop="drawer = !drawer" />
       <v-toolbar-title>
         <nuxt-link to="/">
@@ -31,7 +31,7 @@
         <v-list-item
           v-for="link in links"
           :key="link.name"
-          :to="link.key"
+          :to="link.path"
           nuxt
         >
           {{ link.name }}
@@ -48,11 +48,11 @@ export default {
       drawer: false,
       links: [
         {
-          link: '/posts',
+          path: '/posts',
           name: 'Posts'
         },
         {
-          link: '/machines/3d-printer',
+          path: '/machines',
           name: 'Machine'
         }
       ]

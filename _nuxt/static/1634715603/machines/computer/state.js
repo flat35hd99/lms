b76a9ade0cdb5e3b9ea28e318d3f92e3cd47d0f2
flat35hd99 <flat35hd99@users.noreply.github.com>
2021-10-20 07:56:16 +0000

    deploy: 366cf565b21305b4ac831c1ef7f944aefc0671f3

--- _nuxt/static/1634715603/machines/computer/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flms\u002F_nuxt\u002Fstatic\u002F1634715603",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmachines\u002Fcomputer",config:{_app:{basePath:"\u002Flms\u002F",assetsPath:"\u002Flms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"97cf0b45"}}}}(null));

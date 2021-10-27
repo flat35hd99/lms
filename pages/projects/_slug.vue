05797a37bcf6ed6c3edc3ddf4473177da73c639d
Wataru Sugiura <flat35hd99@gmail.com>
2021-10-27 16:44:37 +0900

    Add project page

---
+++ pages/projects/_slug.vue
@@ -0,0 +1,26 @@
+<template>
+  <article>
+    <v-img
+      :src="project.thumbnail"
+      class="white--text align-end"
+    >
+      <h1
+        style="text-align: right;"
+      >
+        {{ project.title }}
+      </h1>
+    </v-img>
+    <nuxt-content :document="project" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const project = await $content('projects', params.slug).fetch()
+    return {
+      project
+    }
+  }
+}
+</script>

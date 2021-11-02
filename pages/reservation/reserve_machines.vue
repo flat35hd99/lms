21119e5c2a8be3e8f4f8d75ebd49ea195bca8446
Wataru Sugiura <flat35hd99@gmail.com>
2021-11-02 15:44:01 +0900

    Add dummy form

--- pages/reservation/reserve_machines.vue
+++ pages/reservation/reserve_machines.vue
@@ -1,12 +1,19 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <p>
-        ここにMicrosoftのフォームを埋め込みます。おそらくログインした状態ではないと見られないはずなので、埋め込みも問題ありません。
-      </p>
-      <p>
-        検証次第ではこのページを削除して、このページへの遷移リンクをクリックした際に、フォームのページへ直接遷移させるという方法になるかもしれないです。
-      </p>
+      <iframe
+        width="640px"
+        height="480px"
+        src="https://forms.office.com/Pages/ResponsePage.aspx?id=g7JUf-0iPEqpRHX3VeWDBqdWIaZ3ODFCtGdgkUkYLg5UREJSNUQzQTBBUllJRk5US0VIR1UxUDJIMiQlQCN0PWcu&embed=true"
+        frameborder="0"
+        marginwidth="0"
+        marginheight="0"
+        style="border: none; max-width:100%; max-height:100vh"
+        allowfullscreen
+        webkitallowfullscreen
+        mozallowfullscreen
+        msallowfullscreen
+      />
     </v-col>
   </v-row>
 </template>

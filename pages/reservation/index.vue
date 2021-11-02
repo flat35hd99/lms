2bf7c68f0ff39b06b09c54b6fee9c731d7480bc7
Wataru Sugiura <flat35hd99@gmail.com>
2021-11-02 16:48:58 +0900

    Add timeline of start using

--- pages/reservation/index.vue
+++ pages/reservation/index.vue
@@ -31,23 +31,30 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col
-        cols="12"
-      >
-        <h2
-          id="for_beginner"
-        >
-          初めてご利用の方へ
-        </h2>
-        <p>
-          ライブラリーメイカースペースは利用者が安全に機器を利用していただくため、複数種の安全講習を行っています。
-          利用者の方には利用をはじめる前に、これらの講習を受けていただき、かつ、機器それぞれの操作説明を受けていただきます。
-        </p>
+      <v-col>
+        <v-card>
+          <v-card-title
+            id="for_beginner"
+          >
+            初めてご利用の方へ
+          </v-card-title>
+          <v-card-text>
+            ライブラリーメイカースペースは利用者が安全に機器を利用していただくため、複数種の安全講習を行っています。
+            利用者の方には利用をはじめる前に、これらの講習を受けていただき、かつ、機器それぞれの操作説明を受けていただきます。
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
         <v-timeline>
-          <v-timeline-item>NUCTで講習を受ける</v-timeline-item>
-          <v-timeline-item>ライブラリーメイカースペース営業時間中に訪問し、直接講習を受ける</v-timeline-item>
-          <v-timeline-item>ウェブ予約</v-timeline-item>
-          <v-timeline-item>利用開始</v-timeline-item>
+          <v-timeline-item
+            v-for="event in events"
+            :key="event.title"
+          >
+            <v-card>
+              <v-card-title v-text="event.title" />
+              <v-card-text v-text="event.text" />
+            </v-card>
+          </v-timeline-item>
         </v-timeline>
       </v-col>
     </v-row>
@@ -70,6 +77,24 @@ export default {
           to: '/reservation/reserve_machines',
           img: '/reservation/form.png',
           text: 'すでにご利用されている方・講習がお済みのかたはこちらへ'
+        }
+      ],
+      events: [
+        {
+          title: 'NUCTで講習',
+          text: '「一般安全教育」と「実験一般安全教育」の2つを受講し、小テストに合格してください。'
+        },
+        {
+          title: '直接講習',
+          text: 'NUCTの講習受講後、ライブラリーメイカースペースを訪問して職員から講習を受けてください。'
+        },
+        {
+          title: '予約',
+          text: '利用には予約が必要です。機構IDを取得済みであれば、ウェブ予約できます。'
+        },
+        {
+          title: '利用開始',
+          text: '予約した時刻にライブラリーメイカースペースに来て、利用したい機材について操作説明を受けます。'
         }
       ]
     }

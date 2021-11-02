d38dd505230adc01e0a4616232f8cad73fe7c387
flat35hd99 <flat35hd99@users.noreply.github.com>
2021-11-02 07:49:29 +0000

    deploy: 2bf7c68f0ff39b06b09c54b6fee9c731d7480bc7

--- _nuxt/static/1635838320/machines/computer/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flms\u002F_nuxt\u002Fstatic\u002F1635838320",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmachines\u002Fcomputer",config:{_app:{basePath:"\u002Flms\u002F",assetsPath:"\u002Flms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a3d445e9"}}}}(null));
---
+++ _nuxt/static/1635839360/machines/3d-printer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flms\u002F_nuxt\u002Fstatic\u002F1635839360",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmachines\u002F3d-printer",config:{_app:{basePath:"\u002Flms\u002F",assetsPath:"\u002Flms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d87dabb9"}}}}(null));

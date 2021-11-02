d7dc099d17b6315e6a59a931c676236e35eedb94
Wataru Sugiura <flat35hd99@gmail.com>
2021-11-02 17:00:34 +0900

    Update form view

--- pages/reservation/reserve_machines.vue
+++ pages/reservation/reserve_machines.vue
@@ -1,19 +1,33 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <iframe
-        width="640px"
-        height="480px"
-        src="https://forms.office.com/Pages/ResponsePage.aspx?id=g7JUf-0iPEqpRHX3VeWDBqdWIaZ3ODFCtGdgkUkYLg5UREJSNUQzQTBBUllJRk5US0VIR1UxUDJIMiQlQCN0PWcu&embed=true"
-        frameborder="0"
-        marginwidth="0"
-        marginheight="0"
-        style="border: none; max-width:100%; max-height:100vh"
-        allowfullscreen
-        webkitallowfullscreen
-        mozallowfullscreen
-        msallowfullscreen
-      />
-    </v-col>
+  <v-row
+    class="Form"
+  >
+    <iframe
+      src="https://forms.office.com/Pages/ResponsePage.aspx?id=g7JUf-0iPEqpRHX3VeWDBqdWIaZ3ODFCtGdgkUkYLg5UREJSNUQzQTBBUllJRk5US0VIR1UxUDJIMiQlQCN0PWcu&embed=true"
+      frameborder="0"
+      marginwidth="0"
+      marginheight="0"
+      style="border: none; max-width:100%; max-height:100vh"
+      allowfullscreen
+      webkitallowfullscreen
+      mozallowfullscreen
+      msallowfullscreen
+    />
   </v-row>
 </template>
+
+<style>
+.Form {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  padding-top: 56.25%;
+}
+.Form iframe {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>

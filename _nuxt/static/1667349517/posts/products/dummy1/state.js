2190949714eecc89f53ccef313ff5dfffffc4bcd
flat35hd99 <flat35hd99@users.noreply.github.com>
2022-11-02 00:42:29 +0000

    deploy: 63222a238c9388ec7456e6d2ee2cb23cfd72f84a

--- _nuxt/static/1667349517/posts/products/dummy1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flms\u002F_nuxt\u002Fstatic\u002F1667349517",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fproducts\u002Fdummy1",config:{_app:{basePath:"\u002Flms\u002F",assetsPath:"\u002Flms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"10def763"}}}}(null));
---
+++ _nuxt/static/1667349743/posts/news/dummy1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flms\u002F_nuxt\u002Fstatic\u002F1667349743",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fnews\u002Fdummy1",config:{_app:{basePath:"\u002Flms\u002F",assetsPath:"\u002Flms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"042d2b21"}}}}(null));

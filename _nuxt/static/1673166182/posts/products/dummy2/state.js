254a4440287dd14a3a31a0cd209ad74aa50c38cc
flat35hd99 <flat35hd99@users.noreply.github.com>
2023-01-08 08:23:10 +0000

    deploy: b18728ae00c731da3033de66dc9b2eaffa385258

---
+++ _nuxt/static/1673166182/posts/products/dummy2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flms\u002F_nuxt\u002Fstatic\u002F1673166182",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fproducts\u002Fdummy2",config:{_app:{basePath:"\u002Flms\u002F",assetsPath:"\u002Flms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"14ae0bfa"}}}}(null));
